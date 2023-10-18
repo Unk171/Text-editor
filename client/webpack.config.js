5e9901c29c89e0c5e1189cbc2d05557762691d28
Unk171 <7353687@gmail.com>
2023-10-18 12:19:30 -0400

    solved install problem

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,27 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Contact Cards'
+        template: "./index.html",
+        title: "Contact Cards",
       }),
       new WebpackPwaManifest({
         fingerprints: false,
@@ -34,43 +31,34 @@ module.exports = () => {
         description: "Jate",
         background_color: "#ffffff",
         theme_color: "#2196F3",
-        start_url: './',
-        publicPath: './',
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
             src: path.resolve("./src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            sizes: [48, 96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
-         ],
-
+    ],
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
-        //         {
-        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        //   use: [
-        //     {
-        //       loader: 'file-loader',
-        //       options: {
-        //         name: 'images/[name].[ext]',
-        //       },
-        //     },
-        //   ],
-        // },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
